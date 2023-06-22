39dcbe8bcef9da78d88f71f04fff0efd6bb75926
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-22 11:00:20 +0300

    uhyu

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function addingEventListener() {
-}
-
-
-// const input =document.getElementById('input').addEventListener('click', ()=>alert('I was clicked'));
-
-
-// // const input = document.getElementById('input').addEventListener('click', () => alert('I was clicked!'));
 
 let pw = document.getElementById('input');
 function addingEventListener(){
-    
+ 
   alert('I was clicked!');
 }
-input.addEventListener('click', addingEventListener);
+pw.addEventListener('click', addingEventListener);
   
